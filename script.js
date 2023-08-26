629843617cf3f35e2d3848d0a45b5839f48fd406
hskeays <hskeays@gmail.com>
2023-08-26 19:42:11 -0400

    Feat: add button to refresh (clear etch a sketch)

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const div = document.getElementById("container");
 const pixels = document.getElementsByClassName("box");
+const shake = document.querySelector("#refresh");
+shake.addEventListener("click", () => window.location.reload());
+
 for (i = 0; i < 4104; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("box")
