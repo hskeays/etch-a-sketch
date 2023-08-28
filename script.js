066ed92d1d576e596337322db210fb5b8efac6c5
hskeays <hskeays@gmail.com>
2023-08-27 20:32:09 -0400

    Feat: add grayscale feature, add styles

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ blackBtn.addEventListener("click", function () {
     setColor('black');
 });
 
+let grayscaleBtn = document.querySelector("#grayscale");
+grayscaleBtn.addEventListener("click", function () {
+    setColor('grayscale');
+});
+
 let randomBtn = document.querySelector("#random");
 randomBtn.addEventListener("click", function () {
     setColor('random');
@@ -29,10 +34,11 @@ chooseSize.addEventListener("click", function () {
 
 function getUserSize() {
     resetGrid();
-    let input = +prompt("Choose a grid size between 2 and 100: ");
+    let input = +prompt(`Choose a number between 1 and 100.
+The larger the number the smaller the pen! `);
     if (input == "") {
         alert("Please choose a number between 2 and 100")
-    } else if (input < 2 || input > 100) {
+    } else if (input < 1 || input > 100) {
         alert("Please choose a number between 2 and 100")
     } else if (isNaN(input)) {
         alert("Please choose a number between 2 and 100")
@@ -44,10 +50,10 @@ function getUserSize() {
 function createGrid(size) {
     let grid = document.querySelector("#innerContainer");
     let gridCount = size * size;
-    
+
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    
+
     for (i = 0; i < gridCount; i++) {
         let gridSquare = document.createElement("div");
         gridSquare.classList.add("squares")
@@ -59,10 +65,22 @@ function createGrid(size) {
 function colorSquares() {
     if (color == "random") {
         this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        if (this.style.opacity < 0.9) {
+            this.style.opacity = 1;
+        }
     } else if (color == "erase") {
         this.style.backgroundColor = "rgb(240, 240, 240)"
+        this.style.opacity = 0.1;
+    } else if (color == "grayscale") {
+        this.style.backgroundColor = "rgb(0, 0, 0)"
+        if (this.style.opacity <= 0.9) {
+            this.style.opacity = +this.style.opacity + 0.1;
+        } else if (this.style.opacity > 0.9) {
+            this.style.opacity = +this.style.opacity - 0.9;
+        }
     } else {
-        this.style.backgroundColor = "black"
+        this.style.opacity = 1;
+        this.style.backgroundColor = "black";
     }
 }
 
@@ -72,5 +90,6 @@ function setColor(colorChoice) {
 
 function resetGrid() {
     let squares = document.querySelectorAll(".squares");
-    squares.forEach(squares => squares.style.backgroundColor = "rgb(240, 240, 240)")
+    squares.forEach(squares => squares.style.backgroundColor = "rgb(240, 240, 240)");
+    squares.forEach(squares => squares.style.opacity = 0.1);
 }
