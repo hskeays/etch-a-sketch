fbcfc0a3b8b272c0548a2e8f82082588bf69b49e
hskeays <hskeays@gmail.com>
2023-08-28 20:44:10 -0400

    Feat: Add ability to click/drag to draw, add style

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 createGrid(16);
+let mouseDown = false
 let color = "black";
-function setDefaultColor(newColor) {
-    color = newColor;
-}
-
+let chooseSize = document.querySelector("#size");
 let colorPicker = document.querySelector('#colorPicker');
+let shakeBtn = document.querySelector("#shake");
+let grayscaleBtn = document.querySelector("#grayscale");
+let randomBtn = document.querySelector("#random");
+let eraserBtn = document.querySelector("#erase");
+
+document.body.onmousedown = () => (mouseDown = true);
+
+document.body.onmouseup = () => (mouseDown = false);
+
 colorPicker.oninput = (e) => setDefaultColor(e.target.value);
 
-let grayscaleBtn = document.querySelector("#grayscale");
 grayscaleBtn.addEventListener("click", function () {
     setColor('grayscale');
 });
 
-let randomBtn = document.querySelector("#random");
 randomBtn.addEventListener("click", function () {
     setColor('random');
 });
 
-let eraserBtn = document.querySelector("#erase");
 eraserBtn.addEventListener("click", function () {
     setColor('erase');
 });
 
-let shakeBtn = document.querySelector("#shake");
 shakeBtn.addEventListener("click", function () {
     resetGrid();
 });
 
-let chooseSize = document.querySelector("#size");
 chooseSize.addEventListener("click", function () {
     let size = getUserSize();
     createGrid(size);
@@ -62,7 +64,16 @@ function createGrid(size) {
     }
 }
 
-function colorSquares() {
+function setDefaultColor(newColor) {
+    color = newColor;
+}
+
+function setColor(colorChoice) {
+    color = colorChoice
+}
+
+function colorSquares(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
     if (color == "random") {
         this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
         if (this.style.opacity < 0.9) {
@@ -82,10 +93,6 @@ function colorSquares() {
         this.style.opacity = 1;
         this.style.backgroundColor = color;
     }
-}
-
-function setColor(colorChoice) {
-    color = colorChoice
 }
 
 function resetGrid() {
